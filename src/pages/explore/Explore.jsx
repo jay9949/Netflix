9b43e157e759b003433c177fed79a635fc4e2a6e
jay suthar <jay37192@gmail.com>
2024-04-26 10:56:54 +0530

    #

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -14,15 +14,15 @@ import Spinner from "../../components/spinner/Spinner";
 let filters = {};
 
 const sortbyData = [
-    { value: "popularity.desc", label: "Popularity Descending" },
-    { value: "popularity.asc", label: "Popularity Ascending" },
-    { value: "vote_average.desc", label: "Rating Descending" },
-    { value: "vote_average.asc", label: "Rating Ascending" },
+    { value: "popularity.desc", label: "Popularity High" },
+    { value: "popularity.asc", label: "Popularity Low" },
+    { value: "vote_average.desc", label: "Rating High" },
+    { value: "vote_average.asc", label: "Rating Low" },
     {
         value: "primary_release_date.desc",
-        label: "Release Date Descending",
+        label: "Newly Release Date",
     },
-    { value: "primary_release_date.asc", label: "Release Date Ascending" },
+    { value: "primary_release_date.asc", label: "Old Release Date" },
     { value: "original_title.asc", label: "Title (A-Z)" },
 ];
 
